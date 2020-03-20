ff68b35485103d96e12cbd1e8b3b30740b3c1f58
Victor <rusuvictor30@gmail.com>
2020-03-20 16:57:20 -0400

    Small changes

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ window.addEventListener("scroll", () => {
     var scroll = this.scrollY;
     console.log(scroll)
     if(scroll >= 753){
-        const counters = document.querySelectorAll('.counter');
+        const allCounters = document.querySelectorAll('.counter');
         const speed = 2900;
 
-        counters.forEach(counter => {
+        allCounters.forEach(counter => {
             const updateCount = () => {
-                const target = +counter.getAttribute('data-target');
-                const count = +counter.innerText;
-                const inc = target / speed;
-                if (count < target) {
-                    counter.innerText = Math.ceil(count + inc);
+                const counterNumber = +counter.getAttribute('data-target');
+                const divContent = +counter.innerText;
+                const increaseBy = counterNumber / speed;
+                if (divContent < counterNumber) {
+                    counter.innerText = Math.ceil(divContent + increaseBy);
                     setTimeout(updateCount, 1);
                 } else {
-                    counter.innerText = target;
+                    counter.innerText = counterNumber;
                 }
             };
             updateCount();
