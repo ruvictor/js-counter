00e305aefcfe96d346367063e88e5cd68c5752f7
Victor <rusuvictor30@gmail.com>
2020-03-20 18:20:54 -0400

    Logic changed

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 ///// counters 
-window.addEventListener("scroll", () => {
-    var scroll = this.scrollY;
-    console.log(scroll)
-    if(scroll >= 753){
-        const allCounters = document.querySelectorAll('.counter');
-        const speed = 2900;
+const allDivCounters = document.querySelectorAll('#counter');
+const speed = 200;
+allDivCounters.forEach(counter => {
+    const updateCount = setInterval(() => {
+        const counterNumber = +counter.getAttribute('countto');
+        const divContent = +counter.innerText;
+        const increaseBy = counterNumber / speed;
 
-        allCounters.forEach(counter => {
-            const updateCount = () => {
-                const counterNumber = +counter.getAttribute('data-target');
-                const divContent = +counter.innerText;
-                const increaseBy = counterNumber / speed;
-                if (divContent < counterNumber) {
-                    counter.innerText = Math.ceil(divContent + increaseBy);
-                    setTimeout(updateCount, 1);
-                } else {
-                    counter.innerText = counterNumber;
-                }
-            };
-            updateCount();
-        });
-    }
+        if(divContent < counterNumber)
+            counter.innerHTML = Math.ceil(divContent + increaseBy);
+        else
+            clearInterval(updateCount);
+    }, 1);
 });
