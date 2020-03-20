1aadc7cdc2227c1fcfddf61944136ab155f06a93
Victor <rusuvictor30@gmail.com>
2020-03-20 16:47:46 -0400

    V1 done

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 ///// counters 
-window.addEventListener("scroll", function (event) {
+window.addEventListener("scroll", () => {
     var scroll = this.scrollY;
-    
-    if(scroll >= 899){
-        const counters = document.querySelectorAll('.itemCount');
-        const speed = 2500;
+    console.log(scroll)
+    if(scroll >= 753){
+        const counters = document.querySelectorAll('.counter');
+        const speed = 2900;
 
         counters.forEach(counter => {
             const updateCount = () => {
                 const target = +counter.getAttribute('data-target');
                 const count = +counter.innerText;
-
-                // Lower inc to slow and higher to slow
                 const inc = target / speed;
-
-                // console.log(inc);
-                // console.log(count);
-
-                // Check if target is reached
                 if (count < target) {
-                    // Add inc to count and output in counter
                     counter.innerText = Math.ceil(count + inc);
-                    // Call function every ms
                     setTimeout(updateCount, 1);
                 } else {
                     counter.innerText = target;
                 }
             };
-
             updateCount();
         });
     }
